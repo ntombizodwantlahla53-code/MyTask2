aced01c521280a1ad85824627d96df7cff52d7d4
ntombizodwantlahla53-code <ntombizodwantlahla53@gmail.com>
2026-07-23 00:14:33 +0200

    updated linkoutput for linklist

--- src/components/LinkOutput.ts
+++ src/components/LinkOutput.ts
@@ -1,7 +1,8 @@
-import type{ LinkItemTypes } from "./LinkItem"
-export interface LinkListProps{
-    links:LinkItemTypes[];
-    onDelete:() =>void
-    onUpdate:() =>void
+import type { LinkItemTypes } from "./LinkItem"
 
+export interface LinkListProps {
+    links: LinkItemTypes[];
+    onDelete: (id: number) => void;
+    onUpdate: (updatedLink: LinkItemTypes) => void;
+    onSwitchToForm: () => void;
 }
