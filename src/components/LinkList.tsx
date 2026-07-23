5cd914e4624e3f46994079a48683f201fa9a3163
ntombizodwantlahla53 <ntombizodwantlahla53@gmail.com>
2026-07-23 14:15:50 +0200

    changesOnList

--- src/components/LinkList.tsx
+++ src/components/LinkList.tsx
@@ -4,27 +4,21 @@ import type { LinkItemTypes } from "./LinkItem"
 
 export const LinkList: React.FC<LinkListProps> = ({ links, onDelete, onUpdate, onSwitchToForm }) => {
     const [editingId, setEditingId] = useState<number | null>(null);
-    const [editingtitle, setEditingTitle] = useState('');
-    const [editingdescription, setEditingDescription] = useState('');
-    const [editingurl, setEditingUrl] = useState('');
-    const [editingtags, setEditingTags] = useState('');
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [url, setUrl] = useState('');
+    const [tags, setTags] = useState('');
 
     const handleEditClick = (link: LinkItemTypes) => {
         setEditingId(link.id);
-        setEditingTitle(link.title);
-        setEditingDescription(link.description);
-        setEditingUrl(link.url);
-        setEditingTags(link.tags || '');
+        setTitle(link.title);
+        setDescription(link.description);
+        setUrl(link.url);
+        setTags(link.tags || '');
     };
 
     const handleSave = (id: number) => {
-        onUpdate({
-            id,
-            title: editingtitle,
-            description: editingdescription,
-            url: editingurl,
-            tags: editingtags
-        });
+        onUpdate({id, title, description,url,tags});
         setEditingId(null);
     };
 
@@ -35,26 +29,26 @@ export const LinkList: React.FC<LinkListProps> = ({ links, onDelete, onUpdate, o
             <button onClick={onSwitchToForm} className="switch-btn">+ Add Another Link</button>
 
             {links.length === 0 ? (
-                <p>No links saved yet.</p>
+                <p>No links</p>
             ) : (
                 links.map((link) => (
                     
                     <div key={link.id} className="link-card">
                         {editingId === link.id ? (
                             <div className="edit-box">
-                                <input className="input-field" type="text" value={editingtitle} onChange={(e) => setEditingTitle(e.target.value)} />
-                                <input className="input-field" type="text" value={editingdescription} onChange={(e) => setEditingDescription(e.target.value)} />
-                                <input className="input-field" type="text" value={editingurl} onChange={(e) => setEditingUrl(e.target.value)} />
-                                <input className="input-field" type="text" value={editingtags} onChange={(e) => setEditingTags(e.target.value)} />
+                                <input value={title} onChange ={(e) => setTitle(e.target.value)}/>
+                                <input value={description} onChange ={(e) => setDescription(e.target.value)}/>
+                                <input value={url} onChange ={(e) => setUrl(e.target.value)}/>
+                                <input value={tags} onChange ={(e) => setTags(e.target.value)}/>
                                 <button className="save-btn" onClick={() => handleSave(link.id)}>Save</button>
                                 <button className="cancel-btn" onClick={() => setEditingId(null)}>Cancel</button>
                             </div>
                         ) : (
                             <div>
-                                <h3 className="card-title">{link.title}</h3>
-                                <p className="card-desc">{link.description}</p>
-                                <a className="card-url" href={link.url} target="_blank" rel="noopener noreferrer">{link.url}</a>
-                                <p className="card-tags"><small>Tags: {link.tags}</small></p>
+                                <h3>{link.title}</h3>
+                                <p>{link.description}</p>
+                                <a>{link.url}</a>
+                                <p>{link.tags}</p>
                                 <button className="edit-btn" onClick={() => handleEditClick(link)}>Edit</button>
                                 <button className="delete-btn" onClick={() => onDelete(link.id)}>Delete</button>
                             </div>
