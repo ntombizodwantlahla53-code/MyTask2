e5a46100251827518b2c323c287941e6dfd3be1a
ntombizodwantlahla53-code <ntombizodwantlahla53@gmail.com>
2026-07-24 01:00:42 +0200

    changes on linkform

--- src/components/LinkForm.tsx
+++ src/components/LinkForm.tsx
@@ -1,77 +1,44 @@
-import { useState } from "react"
-import type { LinkFormProps } from "./LinkItem"
+import { useState } from "react";
+import type { LinkFormProps } from "./LinkItem";
 
 export const LinkForm: React.FC<LinkFormProps> = ({ onAddLink }) => {
-const [title, setTitle] = useState('')
-const [description, setDescription] = useState('')
-const [url, setUrl] = useState('')
-const [tags, setTags] = useState('')
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [url, setUrl] = useState("");
+  const [tags, setTags] = useState("");
+  
 
-const handleSubmit = (e: React.FormEvent) => {
+  const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     if (!title || !url) return;
 
     onAddLink({ title, description, url, tags });
 
-    setTitle('');
-    setDescription('');
-    setUrl('');
-    setTags('');
-};
+    setTitle(""); setDescription(""); setUrl(""); setTags("");
+  ("/links");
+  };
 
   return (
     <div className="linkContainer">
     <div className="Topic">
-          <h1 className="form-title">Task2 LinkApp</h1>
+          <h1 className="Mytitle">Task2 LinkApp</h1>
           </div>
-      
-          <form onSubmit={handleSubmit} className="todo-wrapper">
-            <div className="divv">
-              <div className="linkss">
-              <div>
-              <label>Title</label>
-              <input 
-                type="text"
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-                placeholder="Enter Text Here"
-                className="input-field"/>
-            </div>
-            <div>
-              <label>Description</label>
-              <input 
-                type="text"
-                value={description}
-                onChange={(e) => setDescription(e.target.value)}
-                placeholder="Enter Description Text"
-                className="input-field"/>
-            </div>
-<div>
-              <label>URL</label>
-              <input 
-                type="text"
-                value={url}
-                onChange={(e) => setUrl(e.target.value)}
-                placeholder="Enter Your Link"
-                className="input-field"/>
-            </div>
-            <div>
-              <label>Tags</label>
-              <input 
-                type="text"
-                value={tags}
-                onChange={(e) => setTags(e.target.value)}
-                placeholder="Your Tag here"
-                className="input-field"/>
-            </div>
-            </div>
-            
-              <button className= "MyButton">Add</button> 
-              
-            
-          </div>
-        </form>
-     
+    <form onSubmit={handleSubmit}>
+      <div className="divv">
+        <div className="linkss">
+          <label>Title</label>
+          <input value={title} onChange={e => setTitle(e.target.value)} placeholder="Add Your Title here" required />
+          <label>Description</label>
+          <input value={description} onChange={e => setDescription(e.target.value)} placeholder="Add Your Description here" />
+          <label>Url</label>
+          <input value={url} onChange={e => setUrl(e.target.value)} placeholder="Add Your URL/Link here" required />
+          <label>Tags</label>
+          <input value={tags} onChange={e => setTags(e.target.value)} placeholder="Add Tag(it is optional)" />
+      </div>
+      <button className= "MyButton"type="submit">Add</button>
+    
+      </div>
+    </form>
     </div>
-  )
-}
+  );
+};
