a9e9991308111d26a5abb77435b7f0ce38df9832
ntombizodwantlahla53 <ntombizodwantlahla53@gmail.com>
2026-07-24 15:44:21 +0200

    updatedAppTsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
 import React, { useState, useEffect } from "react";
 import './App.css'
-import { LinkForm } from "./components/LinkForm";
-import { LinkList } from "./components/LinkList";
+import { LinkForm } from "./components/Form/LinkForm";
+import { LinkList } from "./components/LinkF/LinkList";
 import type { LinkItemTypes } from "./components/LinkItem";
 
 const App: React.FC = () => {
   const [links, setLinks] = useState<LinkItemTypes[]>([]);
   const [showList, setShowList] = useState(false);
 
-  useEffect(() => {
-    const saved = localStorage.getItem("links");
-    if (saved) setLinks(JSON.parse(saved));
-  }, []);
+  // useEffect(() => {
+  //   const saved = localStorage.getItem("links");
+  //   if (saved) setLinks(JSON.parse(saved));
+  // }, []);
 
-  useEffect(() => {
-    localStorage.setItem("links", JSON.stringify(links));
-  }, [links]);
+  // useEffect(() => {
+  //   localStorage.setItem("links", JSON.stringify(links));
+  // }, [links]);
 
   const addLink = (link: Omit<LinkItemTypes, "id">) => {
     setLinks([...links, { id: Date.now(), ...link }]);
