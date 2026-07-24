b0c49aba030ef41b1edfbbd4dee94be90f66d967
ntombizodwantlahla53 <ntombizodwantlahla53@gmail.com>
2026-07-24 11:56:39 +0200

    updatedLinklist

--- src/components/LinkF/LinkList.tsx
+++ src/components/LinkF/LinkList.tsx
@@ -25,7 +25,7 @@ export const LinkList: React.FC<LinkListProps> = ({ links, onDelete, onEdit }) =
 
   return (
     <div className={styles.listCcontainer}>
-      <h2>Saved Links</h2>
+      <h2 className={styles.savedL}>Saved Links</h2>
       {links.length === 0 ? (
         <p>No links yet</p>
       ) : (
@@ -33,22 +33,34 @@ export const LinkList: React.FC<LinkListProps> = ({ links, onDelete, onEdit }) =
           <div key={link.id} className={styles.linkCard}>
             {editingId === link.id ? (
               <div className={styles.box}>
+                <div className={styles.editPage}>
+                  <label>title</label>
                 <input value={title} onChange={e => setTitle(e.target.value)} />
+                <label>description</label>
                 <input value={description} onChange={e => setDescription(e.target.value)} />
+                <label>url</label>
                 <input value={url} onChange={e => setUrl(e.target.value)} />
+                <label>tags</label>
                 <input value={tags} onChange={e => setTags(e.target.value)} />
+                </div>
+                <div>
                 <button className={styles.MySaveBtn} onClick={() => handleSave(link.id)}>Save</button>
                 <button className={styles.MyCancelBtn} onClick={() => setEditingId(null)}>Cancel</button>
               </div>
+              </div>
             ) : (
               
-              <div>
+              <div className={styles.linkbox}>
+                <div className={styles.linksOutp}>
                 <h3>{link.title}</h3>
                 <p>{link.description}</p>
                 <a href={link.url} target="_blank" rel="noopener noreferrer">{link.url}</a>
                 <p>{link.tags}</p>
+                </div>
+                <div>
                 <button className={styles.MyEditBtn} onClick={() => handleEditClick(link)}>Edit</button>
                 <button className={styles.MyDeleteBtn} onClick={() => onDelete(link.id)}>Delete</button>
+              </div>
               </div>
             )}
           </div>
