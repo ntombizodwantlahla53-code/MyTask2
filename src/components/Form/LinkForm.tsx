cb10312b7e58103e197701fde2ac1bfe2c2cd610
ntombizodwantlahla53-code <ntombizodwantlahla53@gmail.com>
2026-07-31 22:20:13 +0200

    added trimWith Validation

--- src/components/Form/LinkForm.tsx
+++ src/components/Form/LinkForm.tsx
@@ -10,49 +10,73 @@ export const LinkForm: React.FC<LinkFormProps> = ({ onAddLink, onView }) => {
   const [url, setUrl] = useState("");
   const [tags, setTags] = useState("");
   
-
-
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    if (!title || !url) return;
 
-    onAddLink({ id: Date.now(), title, description, url, tags });
+    const Title = title.trim();
+    const Description = description.trim();
+    const Url = url.trim();
+    const Tags = tags.trim();
 
+    if (!Title) {
+      alert("Title cannot be empty.Plz Enter Title");
+      return;
+    }
+    if (!/^[A-Za-z\s]+$/.test(Title)) {
+      alert("Title must contain text only.");
+      return;
+    }
+    if (!Description) {
+      alert("Description cannot be empty.Enter Description please!");
+      return;
+    }
+    if (!Url) {
+      alert("URL can't be empty.");
+      return;
+    }
 
-    setTitle(""); setDescription(""); setUrl(""); setTags("");
-  
+    if (!Url.includes("http")) {
+      alert("Please enter a valid URL.");
+      return;
+    }
+    onAddLink({ id: Date.now(), title: Title, description: Description, url: Url, tags: Tags });
+
+    setTitle(""); 
+    setDescription(""); 
+    setUrl(""); 
+    setTags("");
   };
 
   return (
     <div className={styles.linkContainer}>
-    <div className={styles.Topic}>
-          <h1 className={styles.Mytitle}>LinkVault</h1>
-          </div>
-    <form onSubmit={handleSubmit}>
-      <div className={styles.insideContainer}>
-        <div className={styles.linkss}>
-        <div className={styles.title}>
-          <label>Title</label>
-          <input className={styles.inputtitle} value={title} onChange={e => setTitle(e.target.value)} placeholder="Add Your Title here" required />
-          </div>
-          <div className={styles.desc}>
-          <label>Description</label>
-          <input className={styles.inputdesc} value={description} onChange={e => setDescription(e.target.value)} placeholder="Add Your Description here" />
-          </div>
-          <div className={styles.url}>
-          <label>Url</label>
-          <input className={styles.inputurl} value={url} onChange={e => setUrl(e.target.value)} placeholder="Add Your URL/Link here" required />
-          </div>
-          <div className={styles.tags}>
-          <label>Tags</label>
-          <input className={styles.inputtags} value={tags} onChange={e => setTags(e.target.value)} placeholder="Add Tag(it is optional)" />
+      <div className={styles.Topic}>
+        <h1 className={styles.Mytitle}>LinkVault</h1>
       </div>
-      
-      <button className={styles.MyButton}><IoIosAddCircle/>Add</button>
-      <button type="button" className={styles.MyButton} onClick={onView}><CiSaveDown2/>View Links</button>
-    </div>
-      </div>
-    </form>
+      <form onSubmit={handleSubmit}>
+        <div className={styles.insideContainer}>
+          <div className={styles.linkss}>
+            <div className={styles.title}>
+              <label>Title</label>
+              <input className={styles.inputtitle} value={title} onChange={e => setTitle(e.target.value)} placeholder="Add Your Title here" />
+            </div>
+            <div className={styles.desc}>
+              <label>Description</label>
+              <input className={styles.inputdesc} value={description} onChange={e => setDescription(e.target.value)} placeholder="Add Your Description here" />
+            </div>
+            <div className={styles.url}>
+              <label>Url</label>
+              <input className={styles.inputurl} value={url} onChange={e => setUrl(e.target.value)} placeholder="Add Your URL/Link here" />
+            </div>
+            <div className={styles.tags}>
+              <label>Tags</label>
+              <input className={styles.inputtags} value={tags} onChange={e => setTags(e.target.value)} placeholder="Add Tag(it is optional)" />
+            </div>
+          
+            <button className={styles.MyButton}><IoIosAddCircle/>Add</button>
+            <button type="button" className={styles.MyButton} onClick={onView}><CiSaveDown2/>View Links</button>
+          </div>
+        </div>
+      </form>
     </div>
   );
 };
