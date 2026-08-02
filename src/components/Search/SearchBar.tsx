565eea4859ffa70b0cc27cbde7b1fdc067ca7593
ntombizodwantlahla53-code <ntombizodwantlahla53@gmail.com>
2026-08-02 02:33:18 +0200

    SearchBar added

--- src/components/Search/SearchBar.tsx
+++ src/components/Search/SearchBar.tsx
@@ -3,22 +3,21 @@ import { Text } from '../Text/Text'
 import style from './Search.module.css'
 
 export interface SearchBarProps{
-  searchQuery : string
-  onSearch : (newValue: string)=> void
+    searchQuery : string
+    onSearch : (newValue: string)=> void
 
 }
 
 export const Searchbar: React.FC<SearchBarProps> = ({searchQuery, onSearch}) => {
-
-  return (
-
-    <div>
-       <Text variant= {'span'} style={{color: 'rgb(20,20,20)', padding: '10px'}}>Search</Text>
-        <input type = 'text' className ={style.input} value={searchQuery} onChange={(e)=> {
-          onSearch(e.target.value)
-        }} />
-        <FaSearch  className= {style.icon}/>
-    </div>
-
-  )
+    
+    return (
+    
+    <div>
+        <Text variant= {'span'} style={{color: 'rgb(253, 251, 251)', padding: '10px'}}>Search</Text>
+        <input type = 'text' className ={style.input} value={searchQuery} onChange={(e)=> {
+            onSearch(e.target.value)
+            }} />
+            <FaSearch  className= {style.icon}/>
+            </div>
+)
 }
