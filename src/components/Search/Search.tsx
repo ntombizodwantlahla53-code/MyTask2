fb0e4f96140a755daed5f90be7146c24d06b38be
ntombizodwantlahla53-code <ntombizodwantlahla53@gmail.com>
2026-08-02 02:32:12 +0200

    addedSearchFile

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,15 +1,15 @@
 import { Searchbar } from '../Search/SearchBar'
 
 export interface SearchProps{
-  searchQuery : string
-  onSearch : (newValue: string)=> void
+    searchQuery : string
+    onSearch : (newValue: string)=> void
 
 }
 
 export const Search: React.FC<SearchProps> = ({searchQuery,onSearch}) => {
-  return (
-    <div>
-        <Searchbar searchQuery={searchQuery}  onSearch={onSearch}/>
-    </div>
-  )
+    return (
+    <div>
+        <Searchbar searchQuery={searchQuery}  onSearch={onSearch}/>
+        </div>
+        )
 }
